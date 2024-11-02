fabdcdf400b85b16407b408dfc1680b214902ac7
riyam03 <130777469+riyam03@users.noreply.github.com>
2024-11-02 19:24:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+const todoRoutes = require("./routes/todos");
+app.use("/api/v1",todoRoutes);
+app.listen(PORT,()=>{
+    console.log(`Server started at ${PORT}`);
+})
+const dbConnect = require("./config/database");
+dbConnect();
+
+app.get("/",(req,res)=>{
+    res.send(`<h1>this is HOMEPAGE</h1>`);
+});
